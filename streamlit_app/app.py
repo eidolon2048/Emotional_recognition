2975e959b9896e5ff4d474a90229f86a9b85ce0c
eidolon2048 <120173528+eidolon2048@users.noreply.github.com>
2023-04-06 17:51:38 +0200

    test

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -41,26 +41,30 @@ RTC_CONFIGURATION = RTCConfiguration(
 
 cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-class VideoProcessor:
-	def recv(self, frame):
-		frm = frame.to_ndarray(format="bgr24")
-		faces = cv2.cvtColor(frm, cv2.COLOR_RGB2BGR)
+# class VideoProcessor:
+# 	def recv(self, frame):
+# 		frm = frame.to_ndarray(format="bgr24")
+# 		faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
 
-		#faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
-        
+# 		for x,y,w,h in faces:
+# 			cv2.rectangle(frm, (x,y), (x+w, y+h), (0,255,0), 3)
 
-		for x,y,w,h in faces:
-			cv2.rectangle(frm, (x,y), (x+w, y+h), (0,255,0), 3)
+# 		return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
-		return av.VideoFrame.from_ndarray(frm, format='bgr24')
-
-webrtc_streamer(key="key", video_processor_factory=VideoProcessor,
-				rtc_configuration=RTC_CONFIGURATION, 
-				media_stream_constraints={"video": True, "audio": False},
-                async_processing=True,
-	)
+# webrtc_streamer(key="key", video_processor_factory=VideoProcessor,
+# 				rtc_configuration=RTC_CONFIGURATION, 
+# 				media_stream_constraints={"video": True, "audio": False},
+#                 async_processing=True,
+# 	)
 
 
+webrtc_ctx = webrtc_streamer(
+    key="TEST",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration=RTC_CONFIGURATION,
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=True,
+)
 
 
 
