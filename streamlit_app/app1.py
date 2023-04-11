0a435bbbefa7fb64cf8497026e728d3ab6919401
eidolon2048 <120173528+eidolon2048@users.noreply.github.com>
2023-04-11 15:27:03 +0200

    any message

--- streamlit_app/app1.py
+++ streamlit_app/app1.py
@@ -2,3 +2,7 @@ import streamlit as st
 from numpy import load
 from numpy import expand_dims
 from matplotlib import pyplot
+
+st.header("Emotional recognition :scream_cat:")
+st.write("Choose any image and get your emoeion")
+uploaded_file = st.file_uploader("Choose an image...")
