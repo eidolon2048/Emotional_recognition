24c41e8022a8f473e81f7a5d7688f80f3a3b9041
eidolon2048 <120173528+eidolon2048@users.noreply.github.com>
2023-04-11 15:27:04 +0200

    modified:   streamlit_app/app1.py

--- streamlit_app/app1.py
+++ streamlit_app/app1.py
@@ -3,6 +3,7 @@ from numpy import load
 from numpy import expand_dims
 from matplotlib import pyplot
 
-st.header("Emotional recognition :scream_cat:")
-st.write("Choose any image and get your emoeion")
+st.header("Generate ASCII images using GAN")
+st.write("Choose any image and get corresponding ASCII art:")
+
 uploaded_file = st.file_uploader("Choose an image...")
